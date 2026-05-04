02f68e815c001fe538683d6d82f5c1c3108388ae
Mukesh Kumar Karmkar <muxessoo@gmail.com>
2026-05-04 20:32:19 +0530

    appear the latest post on top

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,14 @@
 require('dotenv').config()
 const app = require('./src/app')
 const connectDB= require('./src/db/db')
+const axios = require('axios');
+
+const URL = `https://mern-fnb-proj3.onrender.com/posts`;
+setInterval(() => {
+  axios.get(URL)
+    .then(() => console.log('Ping successful: Keeping the server awake!'))
+    .catch((err) => console.error('Ping failed:', err.message));
+}, 840000);
 
 connectDB()
 
