1ab7d84105888524482eda6908eae9eb4a1e8173
Mukesh Kumar Karmkar <muxessoo@gmail.com>
2026-05-04 19:51:07 +0530

    updated585jjf55f5d

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,8 +12,10 @@ const App = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Feed />} />
         <Route path="/feed" element={<Feed />} />
         <Route path="/create-post" element={<CreatePost />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
   );
