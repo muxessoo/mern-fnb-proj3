42d9d53cb06edea404796d67751c80f4b1d73ac7
Mukesh Kumar Karmkar <muxessoo@gmail.com>
2026-05-04 20:03:54 +0530

    updated585jjf555f5d

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -1,40 +1,24 @@
-import React from 'react'
-import axios from 'axios'
-import {useNavigate} from 'react-router-dom'
+import React from "react";
+import {
+  HashRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import CreatePost from "./pages/CreatePost.jsx";
+import Feed from "./pages/Feed.jsx";
 
-
-const CreatePost = () => {
-
-    const navigate = useNavigate()
-
-    const handleSubmit = async(e)=>{
-        e.preventDefault()
-
-        const formData = new FormData(e.target)
-        try {
-            await axios.post('https://mern-fnb-proj3.onrender.com/create-post', formData)
-            .then((res)=>{
-                navigate('/feed')
-            })
-        } catch (error) {
-            
-        }
-        
-        
-    }
-
+const App = () => {
   return (
-    <div>
-        <section className="create-post-section">
-            <h1>Create Post</h1>
-            <form onSubmit={handleSubmit} >
-                <input type="file" name='image' accept='image/*' />
-                <input type="text" name='caption' placeholder='Enter caption' />
-                <button type='submit' >Submit</button>
-            </form>
-        </section>
-    </div>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Feed />} />
+        <Route path="/feed" element={<Feed />} />
+        <Route path="/create-post" element={<CreatePost />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default CreatePost
+export default App;
--- frontend/src/pages/Feed.jsx
+++ frontend/src/pages/Feed.jsx
@@ -13,7 +13,7 @@ const Feed = () => {
 
   return (
     <section className="feed-section">
-      <button onClick={() => window.location.href = '/create-post'}>Create</button>
+      <button onClick={() => window.location.href = '/#/create-post'}>Create</button>
       {posts.length > 0 ? (
         posts.map((post) => (
           <div key={post._id} className="post-card">
