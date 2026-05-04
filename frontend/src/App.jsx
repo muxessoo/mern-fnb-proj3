c8f28d28e290737433b9777945ce4cc8ec3559ea
Mukesh Kumar Karmkar <muxessoo@gmail.com>
2026-05-04 15:04:48 +0530

    Fixed CORS error and updated API URL

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,6 @@ const App = () => {
   return (
     <Router>
         <Routes>
-            <Route path='/' element={<Navigate to='/feed' replace />} />
             <Route path='/create-post' element={<CreatePost/>}     />
             <Route path='/feed' element={<Feed/>}      />
         </Routes>
