126bb985e3e24ca95e8ef366bb247d0ce6157d89
Mukesh Kumar Karmkar <muxessoo@gmail.com>
2026-05-04 15:52:25 +0530

    updated585d

--- frontend/src/pages/Feed.jsx
+++ frontend/src/pages/Feed.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 
 const Feed = () => {
   const [posts, setPosts] = useState([]);
@@ -12,6 +14,7 @@ const Feed = () => {
 
   return (
     <section className="feed-section">
+      <button onClick={() => navigate("/create-post")}>Create</button>
       {posts.length > 0 ? (
         posts.map((post) => (
           <div key={post._id} className="post-card">
